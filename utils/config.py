da9c816ea9632da80125e965d8edcb25b98f20bb
hiradsajde <hi@hiradsajde.ir>
2025-03-31 02:07:59 +0330

    proxy debugged

--- utils/config.py
+++ utils/config.py
@@ -11,5 +11,14 @@ config = {
     **default_env,
     **dotenv_values()
 }
-
-client = TelegramClient(config["SESSION_NAME"], config["API_ID"], config["API_HASH"],proxy=(config["PROXY_TYPE"], config["PROXY_IP"],config["PROXY_PORT"]))
+if config["PROXY_TYPE"] != None :
+    client = TelegramClient(
+        config["SESSION_NAME"], 
+        config["API_ID"], config["API_HASH"],
+        proxy=(config["PROXY_TYPE"], config["PROXY_IP"],config["PROXY_PORT"])
+    )
+else :
+    client = TelegramClient(
+            config["SESSION_NAME"], 
+            config["API_ID"], config["API_HASH"],
+    )
